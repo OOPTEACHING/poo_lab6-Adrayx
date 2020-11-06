f539ff33dd8947732432d2ccb0d855ce13d7adc2
Adrayx <mafita288@gmail.com>
2020-11-06 19:06:11 +0200

    modified:   .idea/workspace.xml 	modified:   src/insecte/Albina.java 	new file:   src/insecte/Greier.java 	new file:   src/insecte/Regina.java

--- src/insecte/Albina.java
+++ src/insecte/Albina.java
@@ -1,11 +1,19 @@
 package insecte;
 
 public class Albina extends Insecta {
-    private int k = printInit("Albina.k initializat") + 2;
+    protected int k = printInit("Albina.k initializat") + 2;
 
     public Albina()
     {
         System.out.println("k = " + k);
         System.out.println("j = " + j);
+    }
+
+    private static int x3 = printInit("static Albina.x3 initializat");
+
+    public static void main(String[] args)
+    {
+        System.out.println("Albina constructor");
+        Albina a = new Albina();
     }
 }
